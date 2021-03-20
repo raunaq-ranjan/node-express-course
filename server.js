c14085be3dbbb17cdce6398d36c2069ab1e028f2
Raunak Ranjan <raunaq.ranjan@outlook.com>
2021-03-20 17:02:00 +0530

    add dynamic GET route

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 const mockUserData=[
-    {name:'Mark'},
-    {name:'Jill'}
+    {name:'Astha'},
+    {name:'Raunak'}
 ]
 
 app.get('/users', function(req,res){
@@ -13,6 +13,17 @@ app.get('/users', function(req,res){
             users: mockUserData
         })
 })
+
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
+
+
 
 app.listen(8000, function() {
     console.log("server is running!");
